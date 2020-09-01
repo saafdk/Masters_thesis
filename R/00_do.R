dccd7585580cb71de4ae5fc8f72580cb02ab4f18
saafdk <saafdk@gmail.com>
2020-09-01 10:15:38 +0200

    commiting folders and scripts workflow

---
+++ R/00_do.R
@@ -0,0 +1,5 @@
+# Run all scripts --------------------------------------------------------------
+source(file = "R/01_load_and_clean.R")
+source(file = "R/02_augment.R")
+source(file = "R/03_analysis_descriptive.R")
+source(file = "R/04_analysis.R")
